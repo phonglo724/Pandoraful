9a6b5b2655757598b9a7a52d2243dc614542a3c0
Phong Lo <phonglo724@gmail.com>
2021-04-01 16:36:14 -0600

    backend commit 2

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -6,4 +6,10 @@ class RegionsController < ApplicationController
         render json: @regions, include: [:creature, :plant]
     end
 
+    def show 
+        @region = Region.find(params[:id]) 
+
+        render json: @region
+    end
+
 end
